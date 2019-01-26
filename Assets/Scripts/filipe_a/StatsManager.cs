543e6d74efed7d46a7eff97cbb0f50e086411c7d
Filipe <fantunes.dev@gmail.com>
2019-01-26 14:09:55 -0500

    Redid UI, drag and drop prefab to implement

--- Assets/Scripts/filipe_a/StatsManager.cs
+++ Assets/Scripts/filipe_a/StatsManager.cs
@@ -4,72 +4,67 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+[ExecuteInEditMode]
 public class StatsManager : MonoBehaviour
 {
+    public PlayerDataSO playerData;
+
     [Header("Battery")]
+
     public Image batteryImage;
     public Sprite[] batterySprites;
     public TextMeshProUGUI batteryPercentageText;
 
     private int batteryState = 3;
-    //Remove serializeField, it's just for the easy debug
-    [SerializeField] [Range(0, 100)]
     private int batteryPercentageValue = 100;
 
-    [Header("Fuel")]
+    /*[Header("Fuel")]
+
     public Image fuelImage;
     public TextMeshProUGUI fuelPercentageText;
 
-    [SerializeField] [Range(0, 100)]
-    private float fuelPercentageValue = 100;
+    private float fuelPercentageValue = 100;*/
 
-    [Header("Shield")]
-    public Image[] shields;
+    [Header("Life")]
 
-    [SerializeField] [Range(0, 6)]
-    private int shieldState = 6;
+    public Image[] heartImages;
+    public TextMeshProUGUI lifePercentageText;
 
+    private int lifeState = 6;
+    private int lifePercentageValue = 100;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         UpdateBattery();
-        UpdateFuel();
-        UpdateShields();
+        //UpdateFuel();
+        UpdateLife();
     }
 
-    void UpdateFuel()
-    {
-        fuelPercentageText.text = (int)fuelPercentageValue + "\n%";
-        fuelImage.fillAmount = fuelPercentageValue / 100;
-    }
 
     void UpdateBattery()
     {
-        batteryState = (batteryPercentageValue / 25);
+        batteryPercentageValue = Mathf.Clamp(playerData.m_battery, 0, playerData.m_maxBattery);
         batteryPercentageText.text = batteryPercentageValue + "\n%";
+        batteryState = (batteryPercentageValue * 4 / playerData.m_maxBattery) + 1;
+        if (batteryPercentageValue <= 0) { batteryState = 0; }
 
         switch (batteryState)
         {
-            case 0:
-            case 1:
+            case 0:                                            // 0 à 25
                 batteryImage.sprite = batterySprites[0];
                 break;
-            case 2:
+            case 1:
                 batteryImage.sprite = batterySprites[1];
                 break;
-            case 3:
+            case 2:                                            // 25 à 50
                 batteryImage.sprite = batterySprites[2];
                 break;
-            case 4:
+            case 3:                                            // 50 à 75 
                 batteryImage.sprite = batterySprites[3];
+                break;
+            case 4:                                            // 75 à 100
+            case 5:                                            // 100
+                batteryImage.sprite = batterySprites[4];
                 break;
             default:
                 Debug.Log("[Battery] current state not found. Cannot change UI Image");
@@ -77,32 +72,48 @@ public class StatsManager : MonoBehaviour
         }
     }
 
-    void UpdateShields()
+    //void UpdateFuel()
+    //{
+    //    fuelPercentageText.text = (int)fuelPercentageValue + "\n%";
+    //    fuelImage.fillAmount = fuelPercentageValue / 100;
+    //}
+
+    void UpdateLife()
     {
-        switch (shieldState)
+        lifePercentageValue = Mathf.Clamp(playerData.m_life, 0, playerData.m_maxLife);
+        lifePercentageText.text = lifePercentageValue + "\n%";
+        lifeState = (lifePercentageValue * 6 / playerData.m_maxLife) + 1;
+        if (lifePercentageValue <= 0) { lifeState= 0; }
+
+        switch (lifeState)
         {
             case 0:
-                shields[0].fillAmount = 0;
+                foreach (Image heart in heartImages)
+                    heart.fillAmount = 0;
                 break;
             case 1:
-                shields[0].fillAmount = 0.5f;
+                heartImages[0].fillAmount = 0.5f;
                 break;
             case 2:
-                shields[0].fillAmount = 1;
-                shields[1].fillAmount = 0f;
+                heartImages[0].fillAmount = 1;
+                heartImages[1].fillAmount = 0f;
                 break;
             case 3:
-                shields[1].fillAmount = 0.5f;
+                heartImages[1].fillAmount = 1;
+                heartImages[1].fillAmount = 0.5f;
                 break;
             case 4:
-                shields[1].fillAmount = 1;
-                shields[2].fillAmount = 0;
+                heartImages[1].fillAmount = 1;
+                heartImages[2].fillAmount = 0;
                 break;
             case 5:
-                shields[2].fillAmount = 0.5f;
+                heartImages[1].fillAmount = 1;
+                heartImages[2].fillAmount = 0.5f;
                 break;
             case 6:
-                shields[2].fillAmount = 1;
+            case 7:
+                foreach (Image heart in heartImages)
+                    heart.fillAmount = 1;
                 break;
         }
     }

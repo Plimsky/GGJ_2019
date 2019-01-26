9da09d90b1f1c281a4f63f73b8daad4f80362e9f
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2019-01-25 23:04:23 -0500

    Update sprites, add Navigation 2D Navmesh system

--- Assets/Scripts/victor_scripts/TractorBeam.cs
+++ Assets/Scripts/victor_scripts/TractorBeam.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class TractorBeam : MonoBehaviour
 {
     public Transform Crosshair;
-    public Transform TractorBeamStartPoint;
-    
+
     public LayerMask Mask;
-    
+
     private Vector3 MouseLastPos;
 
     public GameObject Target;
 
     private float LastDirTimer;
-    public float DragForce = 1;
+    public  float DragForce = 1;
 
     void Start()
     {
@@ -26,15 +25,15 @@ public class TractorBeam : MonoBehaviour
     {
         Cursor.visible = false;
         Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Crosshair.position= new Vector3(position.x, position.y, 0);
+        Crosshair.position = new Vector3(position.x, position.y, 0);
 
-        Vector3 RayDir = position - TractorBeamStartPoint.position;
-        RaycastHit2D hit = Physics2D.Raycast(TractorBeamStartPoint.position, RayDir, Mask);
+        Vector3      RayDir = position - transform.position;
+        RaycastHit2D hit    = Physics2D.Raycast(transform.position, RayDir, Mask);
 
         //TRACTOR
-        Debug.DrawRay(TractorBeamStartPoint.position, position - TractorBeamStartPoint.position, Color.blue);
+        Debug.DrawRay(transform.position, position - transform.position, Color.blue);
 
-        if(Time.time > LastDirTimer)
+        if (Time.time > LastDirTimer)
         {
             MouseLastPos = Input.mousePosition;
 
@@ -42,29 +41,27 @@ public class TractorBeam : MonoBehaviour
         }
 
         Vector3 MouseDir = Input.mousePosition - MouseLastPos;
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
         {
-            if(hit)
+            if (hit)
             {
                 Target = hit.collider.gameObject;
             }
 
-            if(Target)
+            if (Target)
             {
                 Target.GetComponent<Rigidbody2D>().AddForce(MouseDir * DragForce);
                 Target.GetComponent<Rigidbody2D>().AddTorque(0.01f);
             }
         }
-        else if(!Input.GetMouseButton(0))
+        else if (!Input.GetMouseButton(0))
         {
             Target = null;
         }
-
-        
     }
 
     public void SetForceAppliedToWaste(float force)
     {
-            DragForce = force;
+        DragForce = force;
     }
 }

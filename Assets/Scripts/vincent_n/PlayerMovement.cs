6d1c1e62e32acc46b25bb500ff47a93e8f37d0fa
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2019-01-26 23:18:50 -0500

    Update Player sprite

--- Assets/Scripts/vincent_n/PlayerMovement.cs
+++ Assets/Scripts/vincent_n/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float m_speed = 50.0f;
+    [SerializeField] private Transform m_bodyPlayer;
+    [SerializeField] private float m_smoothBodyPlayerRotation = 1.0f;
+
     private Rigidbody2D m_rigidbody;
     private Vector2 m_velocity;
 
@@ -14,7 +17,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 playerDirection;
+        Vector2 playerDirection = Vector2.zero;
 
         if (AudioManager.instance != null && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
             AudioManager.instance.SetIsPropulsion(true);
@@ -23,6 +26,14 @@ public class PlayerMovement : MonoBehaviour
 
         playerDirection.x = Input.GetAxis("Horizontal");
         playerDirection.y = Input.GetAxis("Vertical");
+
+
+        if (playerDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(playerDirection.y, playerDirection.x) * Mathf.Rad2Deg;
+            m_bodyPlayer.rotation = Quaternion.Slerp(m_bodyPlayer.rotation, Quaternion.Euler(new Vector3(0, 0, angle + 90)), Time.deltaTime * m_smoothBodyPlayerRotation);
+        }
+
         playerDirection *= m_speed;
 
         m_rigidbody.AddForce(playerDirection, ForceMode2D.Force);

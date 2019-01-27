11dff070f19932b888526542eb87f637dfa7adf4
Victor <victorio56gamers@gmail.com>
2019-01-27 15:05:18 -0500

    Enemy triggerZone done

--- Assets/Scripts/victor_scripts/EnemyTriggerZone.cs
+++ Assets/Scripts/victor_scripts/EnemyTriggerZone.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class EnemyTriggerZone : MonoBehaviour
 {
+
+    public GameObject[] Ennemies;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(GameObject ennemy in Ennemies)
+        {
+            ennemy.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            ActivateEnnemy();
+        }
+    }
+
+
+    void ActivateEnnemy()
+    {
+        foreach(GameObject ennemy in Ennemies)
+        {
+            ennemy.SetActive(true);
+        }
     }
 }
